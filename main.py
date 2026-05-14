11fad79bc4bfaa8f9ba45e81ebaf5909c0419e64
Lucas-Alves-S <lucasalvessouzalima05@gmail.com>
2026-05-14 17:48:10 -0300

    application entrypoint

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+
+from src.maze import Maze
+from src.generators.kruskal import RandomizedKruskal
+from src.solvers.greedy import GreedySolver
+
+
+def main():
+    maze = Maze(
+        rows=15,
+        cols=30,
+        num_entrances=4,
+        num_exits=1,
+        generator=RandomizedKruskal(),
+        solver=GreedySolver(),
+    )
+    maze.generate()
+    maze.solve_and_display(delay=0.1) # TODO: function to call the solver and display the solution step by step
+
+
+if __name__ == "__main__":
+    main()
